a34c5d352b7322eb3fc1e4596604032e55493c46
Hamza <Hamzawaheed123h@gmail.com>
2026-03-18 16:14:33 +0200

    add 3 tasks

---
+++ task3/Program 08.cs
@@ -0,0 +1,21 @@
+﻿namespace o8g
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+           int x= int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            Console.Write("x+y=");
+            Console.WriteLine(x + y);
+            Console.Write("x*y=");
+            Console.WriteLine(x * y);
+            Console.Write("x-y=");
+            Console.WriteLine(x - y);
+
+            Console.ReadKey();
+
+            
+        }
+    }
+}
---
+++ task3/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int z = (x + 1) / 2;
+            int y = x * z;
+            Console.WriteLine(y);
+        }
+    }
+}
+
